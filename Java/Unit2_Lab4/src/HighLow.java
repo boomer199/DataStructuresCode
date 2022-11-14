739a689c8a381aa0127ce48cef9c0ef4af6fa00e
boomer199 <ec.villiger@gmail.com>
2022-11-14 13:16:45 -0800

    work on new lab

---
+++ Java/Unit2_Lab4/src/HighLow.java
@@ -0,0 +1,39 @@
+/**
+   HighLow class
+*/
+
+public class HighLow<T extends Comparable<T>>{
+   private T[] array;
+   
+   /**
+      Constructor
+      @param values An array of values.
+      @throws IllegalArgumentException if values is null or
+              contains no elements.
+   */
+   public HighLow(T[] values){
+      // Validate values.
+      
+         
+      // Values is OK so assign it to array.
+      
+
+   }
+   
+   /**
+      getHighest method
+      @return The highest value in the array.
+   */
+   public T getHighest(){
+      
+   }
+
+   /**
+      getLowest method
+      @return The lowest value in the array.
+   */
+   public T getLowest(){
+      int lowIndex = 0;
+      
+   }
+}
---
+++ Java/Unit2_Lab4/src/HighLowTest.java
@@ -0,0 +1,30 @@
+/**
+   This program demonstrates the HighLow class.
+*/
+
+public class HighLowTest
+{
+   public static void main(String[] args)
+   {
+      // Create an array of Integer values.
+      Integer[] numbers = { new Integer(44),
+                            new Integer(77),
+                            new Integer(11),
+                            new Integer(99),
+                            new Integer(55),
+                            new Integer(22)
+                          };
+      
+      // Create a HighLow object.
+      HighLow<Integer> highLow = new HighLow<>(numbers);
+      
+      // Display the highest value in the array.
+      System.out.println("The highest value is " +
+                          highLow.getHighest());
+
+      // Display the lowest value in the array.
+      System.out.println("The lowest value is " +
+                          highLow.getLowest());
+                          
+   }
+}
