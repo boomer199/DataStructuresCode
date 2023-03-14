91703ab927cfaf676d2539a8b14020f08c5d2784
boomer199 <ec.villiger@gmail.com>
2023-03-14 12:59:51 -0700

    kjdkj

--- Java/Unfinished/Bookstore/src/com/company/Book.java
+++ Java/Unfinished/Bookstore/src/com/company/Book.java
@@ -1,14 +1,29 @@
 package com.company;
 
 public class Book {
+    // Title of book
     public String title;
+    // author of book
     public String author;
+    // ISBN of book
     public String ISBN;
+    // price of book
     public double price;
+    // number of books
     public int quantity;
+    // prev book in linkedlist
     public Book previous;
+    // next book in linkedlist
     public Book next;
 
+    /**
+     * Makes a new book node
+     * @param title of book
+     * @param author of book
+     * @param ISBN of book
+     * @param price of book
+     * @param quantity of books
+     */ 
     public Book(String title, String author, String ISBN, double price, int quantity) {
         this.title = title;
         this.author = author;
--- Java/Unfinished/Bookstore/src/com/company/DoublyLinkedList.java
+++ Java/Unfinished/Bookstore/src/com/company/DoublyLinkedList.java
@@ -1,9 +1,19 @@
 package com.company;
 
 public class DoublyLinkedList {
+    // head of linkedlist
     private Book head;
+    // tail of linkedlist
     private Book tail;
 
+    /**
+     * Adds a book to doubly linked list
+     * @param title of book 
+     * @param author of book
+     * @param ISBN of book
+     * @param price of book
+     * @param quantity of bools
+     */
     public void addBook(String title, String author, String ISBN, double price, int quantity) {
         Book book = new Book(title, author, ISBN, price, quantity);
         if (head == null) {
@@ -16,6 +26,10 @@ public class DoublyLinkedList {
         }
     }
 
+    /**
+     * removes book from linked list given isbn 
+     * @param ISBN of book
+     */
     public void removeBook(String ISBN) {
         Book book = searchBook(ISBN);
         if (book != null) {
@@ -32,6 +46,11 @@ public class DoublyLinkedList {
         }
     }
 
+    /**
+     * Updates price of book given ISBN
+     * @param ISBN of book changed
+     * @param price new price of book
+     */
     public void updatePrice(String ISBN, double price) {
         Book book = searchBook(ISBN);
         if (book != null) {
@@ -39,6 +58,11 @@ public class DoublyLinkedList {
         }
     }
 
+    /**
+     * Updates quantity of books given isbn
+     * @param ISBN of book
+     * @param quantity new quantity of book
+     */
     public void updateQuantity(String ISBN, int quantity) {
         Book book = searchBook(ISBN);
         if (book != null) {
@@ -46,6 +70,12 @@ public class DoublyLinkedList {
         }
     }
 
+    /**
+     * Searches for a book in the linked list given isbn
+     * @param ISBN of book
+     * @return null if the book isnt found
+     * @return the book if it is found
+     */
     public Book searchBook(String ISBN) {
         Book current = head;
         while (current != null) {
@@ -57,6 +87,9 @@ public class DoublyLinkedList {
         return null;
     }
 
+    /**
+     * Displays all books in the linkedlists 
+     */
     public void displayInventory() {
         Book current = head;
         while (current != null) {
@@ -70,6 +103,9 @@ public class DoublyLinkedList {
         }
     }
 
+    /**
+     * Displays all books in inventory in reverse
+     */
     public void displayInventoryReverse() {
         Book current = tail;
         while (current != null) {
